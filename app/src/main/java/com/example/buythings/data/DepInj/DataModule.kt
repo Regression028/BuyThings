d2e73dbe42f7b62e99a31acafabd840fc5afbc79
GAURAV BANSAL <gauravbansal2627@gmail.com>
2026-09-03 04:31:07 +0530

    integrating razorpay

--- app/src/main/java/com/example/buythings/data/DepInj/DataModule.kt
+++ app/src/main/java/com/example/buythings/data/DepInj/DataModule.kt
@@ -46,7 +46,7 @@ object DataModule {
             .build()
 
         return Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:5001/")
+            .baseUrl("http://127.0.0.1:5001/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
